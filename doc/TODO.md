8440dc66360be5317fd77d66f63c7478572924c0
Davi <davisantosdias@protonmail.com>
2025-01-04 13:29:55 -0300

    adding main application files and files of modules and annotations

---
+++ doc/TODO.md
@@ -0,0 +1,19 @@
+Ótimo, Davi! Vamos começar com um projeto básico para criar uma API simples usando o **Gin**. Aqui está o plano para o primeiro projeto:
+
+### Projeto 1: API de Gerenciamento de Tarefas (CRUD)
+**Objetivo:** Construir uma API que permita criar, ler, atualizar e deletar tarefas.
+
+#### Funcionalidades:
+1. **Criar Tarefa**: Endpoint para adicionar uma nova tarefa (com título e descrição).
+2. **Listar Tarefas**: Endpoint para retornar todas as tarefas.
+3. **Atualizar Tarefa**: Endpoint para editar o título ou descrição de uma tarefa pelo ID.
+4. **Deletar Tarefa**: Endpoint para remover uma tarefa pelo ID.
+
+#### Passos:
+
+- [x] Configurar um projeto Go com o pacote Gin.
+- [x] Criar os endpoints para cada funcionalidade acima.
+- [x] Armazenar as tarefas em memória (simples slice ou map) para este projeto inicial.
+- [x] Testar os endpoints com ferramentas como Postman ou cURL.
+
+Vamos começar configurando seu ambiente e criando o primeiro endpoint?
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"api-tarefas/configs"
+	"api-tarefas/routes"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db, err := configs.ConnectionDB()
+
+	if err != nil {
+		log.Fatalf("Erro ao conectar ao banco de dados: %v", err)
+	}
+	defer db.Close()
+
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},            // Métodos HTTP permitidos
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Headers permitidos
+		ExposeHeaders:    []string{"Content-Length"},                          // Headers visíveis para o cliente
+		AllowCredentials: true,                                                // Permitir cookies e credenciais
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// registrar rotas
+	routes.TaskRoute(r, db)
+
+	if err := r.Run(":3000"); err != nil {
+		fmt.Println("Erro ao iniciar servidor")
+	}
+}
